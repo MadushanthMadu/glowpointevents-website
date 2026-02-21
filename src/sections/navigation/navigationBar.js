3b3642e351d4f547d840c42a431acef8f04dcd1c
Prasanth <prasanth@techserw.com>
2026-02-21 22:26:54 +0530

    FIXED: Navigation bar scroll visibility issue

--- src/sections/navigation/navigationBar.js
+++ src/sections/navigation/navigationBar.js
@@ -7,7 +7,7 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { useCallback, useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { alpha, Drawer, Stack, useTheme } from '@mui/material';
+import { alpha, Drawer, Stack, useScrollTrigger, useTheme } from '@mui/material';
 import CustomButton from '../../components/customButton';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import { useTranslation } from 'react-i18next';
@@ -68,13 +68,21 @@ function NavigationBar() {
         navigate(to);
     }, []);
 
+    const trigger = useScrollTrigger({
+        // Disable the delay on scroll direction change
+        disableHysteresis: true, 
+        // Trigger as soon as scrolling starts (can adjust this value)
+        // You can also pass the window object if needed: window: props.window
+        threshold: 0
+    });
+
     return (
         <AppBar 
             position="fixed"
             elevation={0}
             sx={{
-                backgroundColor: 'transparent',
-                pt: {xs: '15px', md: '30px'}
+                backgroundColor: trigger ? theme.palette.background.default : 'transparent',
+                py: {xs: '12px', md: trigger ? '5px' : '15px'}
             }}
             className='main-section-px'
         >
@@ -84,7 +92,7 @@ function NavigationBar() {
             >
                 <Toolbar disableGutters>
                     {/* Logo - PC and Tab View */}
-                    <Logo />
+                    <Logo trigger={trigger}/>
 
                     {/* Navigation Options - Mobile View */}
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -96,7 +104,7 @@ function NavigationBar() {
                             onClick={handleOpenNavMenu}
                             color="inherit"
                         >
-                            <MenuIcon />
+                            <MenuIcon sx={{color: trigger ? theme.palette.text.primary : theme.palette.background.default}}/>
                         </IconButton>
 
                         <Drawer
@@ -178,10 +186,10 @@ function NavigationBar() {
                             spacing='48px'
                             sx={{
                                 backgroundColor: alpha(theme.palette.background.default, 0.2),
-                                boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
-                                backdropFilter: 'blur(5px)',
-                                WebkitBackdropFilter: 'blur(5px)',
-                                py: '12px',
+                                boxShadow: trigger ? 'none' : '0 4px 30px rgba(0, 0, 0, 0.1)',
+                                backdropFilter: trigger ? 'none' : 'blur(5px)',
+                                WebkitBackdropFilter: trigger ? 'none' : 'blur(5px)',
+                                py: trigger ? 0 : '12px',
                                 px: '24px',
                                 borderRadius: '100px'
                             }}
@@ -193,7 +201,7 @@ function NavigationBar() {
                                         onClick={() => selectAPage(page.to)}
                                         sx={{ 
                                             textTransform: 'none',
-                                            color: theme.palette.primary.contrastText, 
+                                            color: trigger ? theme.palette.text.primary : theme.palette.primary.contrastText, 
                                             display: 'block',
                                             fontSize: selectedPageIndex === page.id ? '18px' : '16px',
                                             fontWeight: selectedPageIndex === page.id ? 700 : 400,
@@ -218,8 +226,8 @@ function NavigationBar() {
                             onClick={() => navigate('/contact-us')}
                             endIcon={<ArrowOutwardIcon />}
                             sx={{
-                                backgroundColor: theme.palette.background.default,
-                                color: theme.palette.primary.main,
+                                backgroundColor: trigger ? theme.palette.primary.main : theme.palette.background.default,
+                                color: trigger ? theme.palette.primary.contrastText : theme.palette.primary.main,
                                 fontWeight: 600
                             }}
                         />
@@ -232,7 +240,9 @@ function NavigationBar() {
 
 export default NavigationBar;
 
-const Logo = () => {
+const Logo = ({
+    trigger
+}) => {
     return (
         <>
             <Link
@@ -248,7 +258,7 @@ const Logo = () => {
                     }}
                 >
                     <img 
-                        src='/images/Logo/GE-W-G.png' 
+                        src={trigger ? '/images/Logo/GE-T-G.png' : '/images/Logo/GE-W-G.png'}
                         alt='Logo' 
                         width='100%' 
                     />
